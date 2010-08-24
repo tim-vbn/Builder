b3cf4e43160e1a73be0269b9e7e2bc6b199545df
tim <tim@VBN26.vanbelle.local>
2010-08-24 10:26:19 -0700

    1.3

--- org.vbn.p2.one/src/org/vbn/p2/one/LabelOne.java
+++ org.vbn.p2.one/src/org/vbn/p2/one/LabelOne.java
@@ -9,7 +9,7 @@ public class LabelOne extends JPanel
 {
 	public LabelOne()
 	{
-		JLabel label = new JLabel("One: version 1.2");
+		JLabel label = new JLabel("One: version 1.3");
 		label.setFont(label.getFont().deriveFont(Font.BOLD,24));
 		label.setHorizontalAlignment(SwingConstants.CENTER);
 		label.setVerticalAlignment(SwingConstants.CENTER);
